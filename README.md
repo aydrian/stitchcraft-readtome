b1eb762bb89b3f594a43e1070dcbdb4efe2457e7
Aydrian Howard <aydrian@gmail.com>
2018-11-16 09:04:43 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# Read to Me
+
+An application created using MongoDB Stitch and React.js. It uses AWS Rekognition to detect text in an uploaded image and speaks it using AWS Polly. The Live-coding video for this app can be found on the [StitchCraft YouTube](https://youtu.be/5-ejg3l5Bsg).
+
+[![Powered by Stitch](http://badge.learnstitch.com/?appid=stitchcraft-picstream-kcpaj)](http://cloud.mongodb.com)
+
+## web-ui
+
+React.js application created using [Create React App](https://github.com/facebook/create-react-app).
+
+## stitch-app
+
+Exported (as template) [Stitch app](https://docs.mongodb.com/stitch/import-export/export-stitch-app/).
+
+## Scripts
+
+The following scripts will assist in the initial setup of your Stitch Application.
+
+## Requirements:
+
+- Install the [stitch-cli](https://docs.mongodb.com/stitch/import-export/stitch-cli-reference/)
+- Generate an [API Key](https://docs.atlas.mongodb.com/configure-api-access/#generate-api-keys)
+- Create a `.env` file like the following
+
+```
+export STITCH_API_KEY=<API_KEY>
+export STITCH_USERNAME=<CLOUD_USERNAME>
+export STITCH_APPID=<APPID>
+```
+
+- Create a `secrets.json` file in the `stitch-app` directory like the following
+
+```
+{
+  "services": {
+    "AWS": {
+      "secretAccessKey": "<secret-access-key>",
+      "accessKeyId": "<access-id>"
+    }
+  }
+}
+```
+
+**NOTE:** Do not commit this file.
+
+### deploy.sh
+
+Deploy current code using `./deploy.sh` in the root of the project
+
+```
+> ./deploy.sh
+```
+
+### export.sh
+
+Export the project code template using `./export.sh` in the root of the project.
+
+```
+> ./export.sh
+```
+
+**Note:** This exports the application configuration without any service ID values, including the App ID.
---
+++ web-ui/src/App.js
@@ -0,0 +1,123 @@
+import React, { Component } from 'react'
+import { Stitch, AnonymousCredential } from 'mongodb-stitch-browser-sdk'
+import {
+  AwsServiceClient,
+  AwsRequest
+} from 'mongodb-stitch-browser-services-aws'
+import FileInput from './components/FileInput'
+import './App.css'
+
+const convertImageToBSONBinaryObject = file => {
+  return new Promise(resolve => {
+    var fileReader = new FileReader()
+    fileReader.onload = event => {
+      resolve({
+        $binary: {
+          base64: event.target.result.split(',')[1],
+          subType: '00'
+        }
+      })
+    }
+    fileReader.readAsDataURL(file)
+  })
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.appId = 'stitchcraft-readtome-jhjcq'
+
+    this.state = {
+      image: null,
+      text: '',
+      audio: null
+    }
+
+    this.handleFileUpload = this.handleFileUpload.bind(this)
+    this.audio_ref = React.createRef()
+  }
+
+  componentDidMount() {
+    this.client = Stitch.initializeDefaultAppClient(this.appId)
+    this.client.auth.loginWithCredential(new AnonymousCredential())
+    this.aws = this.client.getServiceClient(AwsServiceClient.factory, 'AWS')
+  }
+
+  handleFileUpload(file) {
+    if (!file) {
+      return
+    }
+
+    this.setState({ image: URL.createObjectURL(file), text: '', audio: null })
+
+    convertImageToBSONBinaryObject(file)
+      .then(result => {
+        console.log(result)
+
+        const args = {
+          Image: {
+            Bytes: result
+          }
+        }
+
+        const request = new AwsRequest.Builder()
+          .withService('rekognition')
+          .withAction('DetectText')
+          .withRegion('us-east-1')
+          .withArgs(args)
+          .build()
+
+        return this.aws.execute(request)
+      })
+      .then(result => {
+        console.log(result.TextDetections)
+        const text = result.TextDetections[0].DetectedText
+        console.log(text)
+        this.setState({ text })
+
+        const args = {
+          OutputFormat: 'mp3',
+          Text: text,
+          VoiceId: 'Brian'
+        }
+
+        const request = new AwsRequest.Builder()
+          .withService('polly')
+          .withAction('SynthesizeSpeech')
+          .withRegion('us-east-1')
+          .withArgs(args)
+          .build()
+
+        return this.aws.execute(request)
+      })
+      .then(result => {
+        console.log(result)
+        const blob = new Blob([result.AudioStream.buffer], {
+          type: result.ContentType
+        })
+        this.setState({ audio: URL.createObjectURL(blob) })
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <FileInput handleFileUpload={this.handleFileUpload} />
+        {this.state.image && (
+          <img width="200" src={this.state.image} alt="uploaded" />
+        )}
+        {this.state.text && <h3>{this.state.text}</h3>}
+        {this.state.audio && (
+          <audio
+            ref={this.audio_ref}
+            src={this.state.audio}
+            autoPlay
+            controls
+          />
+        )}
+      </div>
+    )
+  }
+}
+
+export default App
